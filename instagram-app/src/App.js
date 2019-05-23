485806697c7e628e4f48fc532afe48d01ad3e907
Eric Hendrix <agentmrbig@gmail.com>
2019-05-23 01:48:03 -0400

    started messing with proptypes

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
+import PropTypes from 'prop-types';
 import PostContainer from './components/PostContainer/PostContainer';
-
 
 import './App.css';
 
+const Test = (props) => {
+  return (
+    <h1>{props.str}</h1>
+  )
+}
+
+Test.propTypes = {
+  str:PropTypes.string
+}
+
 class App extends Component {
-  constructor(props) {
-    super(props);
+        constructor(props) {
+      super(props);
     this.state = {
-      //posts: dummyData
-    }
-  }
+        //posts: dummyData
+      }
+      }
   render() {
     return (
       <div className="App">
         <SearchBar />
         <PostContainer />
+        <Test str={'EricHendrix'}/>
       </div>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
